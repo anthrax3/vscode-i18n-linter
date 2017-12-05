26393760f815e871a86d98066b9591b6c6329ed1
jasonslyvia <jasonslyvia@gmail.com>
2017-12-05 16:13:42 +0800

    add template string support

--- src/extension.ts
+++ src/extension.ts
@@ -173,15 +173,35 @@ export function activate(context: vscode.ExtensionContext) {
 	function replaceAndUpdate(arg: TargetStr, val: string, validateDuplicate: boolean): Thenable<any> {
 		const edit = new vscode.WorkspaceEdit();
 		const { document } = vscode.window.activeTextEditor;
+
+		let finalReplaceText = arg.text;
+
 		// 若是字符串，删掉两侧的引号
 		if (arg.isString) {
 			// 如果引号左侧是 等号，则可能是 jsx 的 props，此时要替换成 {
-			const prevTextRange = new vscode.Range(arg.range.start.translate(0, -2), arg.range.start.translate(0, -1));
-			const prevText = document.getText(prevTextRange);
+			const prevTextRange = new vscode.Range(arg.range.start.translate(0, -2), arg.range.start);
+			const [last2Char, last1Char] = document.getText(prevTextRange).split('');
 			let finalReplaceVal = val;
-			if (prevText === '=') {
+			if (last2Char === '=') {
 				finalReplaceVal = '{' + val + '}';
 			}
+
+			// 若是模板字符串，看看其中是否包含变量
+			if (last1Char === '`') {
+				const varInStr = arg.text.match(/(\$\{[^\}]+?\})/g);
+				if (varInStr) {
+					const kvPair = varInStr
+						.map((str, index) => {
+							return `val${index+1}: ${str.replace(/^\${([^\}]+)\}$/, '$1')}`;
+						});
+					finalReplaceVal = `I18N.get('${val.replace(/^I18N\./, '')}', { ${kvPair.join(',\n')} })`;
+
+					varInStr.forEach((str, index) => {
+						finalReplaceText = finalReplaceText.replace(str, `{val${index+1}}`);
+					});
+				}
+			}
+
 			edit.replace(document.uri, arg.range.with({
 				start: arg.range.start.translate(0, -1),
 				end: arg.range.end.translate(0, 1),
@@ -193,7 +213,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		try {
 			// 更新语言文件
-			updateLangFiles(val, arg.text, validateDuplicate);
+			updateLangFiles(val, finalReplaceText, validateDuplicate);
 			// 若更新成功再替换代码
 			return vscode.workspace.applyEdit(edit);
 		}
